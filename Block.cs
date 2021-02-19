a5bcf59545c7ff8b2ecdc860e8f4d84e2068741a
Devyn Smith <devyn.smith17@gmail.com>
2021-02-18 20:09:29 -0700

    Add project files.

---
+++ Block.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using Newtonsoft.Json;
+using EllipticCurve;
+
+namespace RealCoin
+{
+    class Block
+    {
+        public int Index { get; set; }
+        public string PreviousHash { get; set; }
+        public string Timestamp { get; set; }
+        public string Hash { get; set; }
+        public int Nonce { get; set; }
+        public List<Transaction> Transactions { get; set; }
+
+        public Block(int index, string timestamp, List<Transaction> transations, string previousHash = "")
+        {
+            this.Index = index;
+            this.Timestamp = timestamp;
+            this.Transactions = transations;
+            this.PreviousHash = previousHash;
+            this.Hash = CalculateHash();
+            this.Nonce = 0;
+        }
+
+        public string CalculateHash()
+        {
+            string blockData = this.Index + this.PreviousHash + this.Timestamp + this.Transactions.ToString() + this.Nonce;
+            byte[] blockBytes = Encoding.ASCII.GetBytes(blockData);
+            byte[] hashByte = SHA256.Create().ComputeHash(blockBytes);
+            return BitConverter.ToString(hashByte).Replace("-", "");
+        }
+
+        public void Mine(int difficulty)
+        {
+            while (this.Hash.Substring(0,difficulty) != new String('0', difficulty))
+            {
+                this.Nonce++;
+                this.Hash = this.CalculateHash();
+                //Console.WriteLine("Mining: " + this.Hash);
+            }
+
+            Console.WriteLine("BLock has been mined: " + this.Hash);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using Newtonsoft.Json;
+using EllipticCurve;
+
+namespace RealCoin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrivateKey key1 = new PrivateKey();
+            PublicKey wallet1 = key1.publicKey();
+
+            PrivateKey key2 = new PrivateKey();
+            PublicKey wallet2 = key2.publicKey();
+
+            Blockchain realcoin = new Blockchain(6, 100);
+
+            Console.WriteLine("Start the Miner.");
+            realcoin.MinePendingTransactions(wallet1);
+            Console.WriteLine("\nBalance of wallet1 is $" + realcoin.GetBalanceOfWallet(wallet1).ToString());
+
+            Transaction tx1 = new Transaction(wallet1, wallet2, 10);
+            tx1.SignTransaction(key1);
+            realcoin.addPendingTransaction(tx1);
+            Console.WriteLine("Start the Miner.");
+            realcoin.MinePendingTransactions(wallet2);
+            Console.WriteLine("\nBalance of wallet1 is $" + realcoin.GetBalanceOfWallet(wallet1).ToString());
+            Console.WriteLine("\nBalance of wallet2 is $" + realcoin.GetBalanceOfWallet(wallet2).ToString());
+
+
+            string blockJSON = JsonConvert.SerializeObject(realcoin, Formatting.Indented);
+            Console.WriteLine(blockJSON);
+
+            //realcoin.GetLatestBlock().PreviousHash = "12345";
+
+            if (realcoin.IsChainValid())
+            {
+                Console.WriteLine("Blockchain is Valid!!");
+            }
+            else
+            {
+                Console.WriteLine("Blockchain is NOT Valid.");
+            }
+               
+        }
+    }
+}
